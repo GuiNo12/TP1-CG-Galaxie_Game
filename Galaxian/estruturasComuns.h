b4a1a464c6421489396ec7e87f98bbbe868692bd
GuiNo12 <guilherme.nogueiracoelho@live.com>
2022-05-12 08:50:00 -0300

    Remoção de arquivo base

--- Galaxian/estruturasComuns.h
+++
@@ -1,4 +0,0 @@
-/*
-    Estruturas comuns a todos os arquivos
-        Structs de posição, jogador, etc.
-*/
--- Galaxian/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <SOIL/SOIL.h>
-#include <GL/glew.h>
-#include <GL/freeglut.h>
-#include <stdlib.h>
-#include <stdio.h>
-
